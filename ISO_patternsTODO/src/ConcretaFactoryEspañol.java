914a2daf6d3128ab37ea2d509dbb6ac77fcb92ef
jsalig00 <jsalig00@estudiantes.unileon.es>
2020-10-20 17:53:05 +0200

    concreta factory done

--- ISO_patternsTODO/src/ConcretaFactoryEspañol.java
+++ ISO_patternsTODO/src/ConcretaFactoryEspañol.java
@@ -2,23 +2,23 @@
 public class ConcretaFactoryEspañol {
 	
 	//Atributos
-		private Moneda moneda;
-		private Lista lista;
+		private MonedaEspaña moneda;
+		private ListaEspañol lista;
 		
 		//Constructor 2
 		public ConcretaFactoryEspañol() {
 			
-			this.moneda = new MonedaEspaña();
+			this.moneda = new MonedaEspaña(1.0);
 			this.lista = new ListaEspañol();		
 		}
 		
 		//Metodos
 		
-		public Moneda getMoneda() {
+		public MonedaEspaña getMoneda() {
 			return this.moneda;
 		}
 		
-		public Lista getLista() {
+		public ListaEspañol getLista() {
 			return this.lista;
 		}
 
--- ISO_patternsTODO/src/ConcretaFactoryIngles.java
+++ ISO_patternsTODO/src/ConcretaFactoryIngles.java
@@ -2,23 +2,23 @@
 public class ConcretaFactoryIngles {
 	
 	//Atributos
-	private Moneda moneda;
-	private Lista lista;
+	private MonedaUSA moneda;
+	private ListaIngles lista;
 	
 	//Constructor
 	public ConcretaFactoryIngles() {
 		
-		this.moneda = new MonedaUSA();
+		this.moneda = new MonedaUSA(1.18);
 		this.lista = new ListaIngles();		
 	}
 	
 	//Metodos
 	
-	public Moneda getMoneda() {
+	public MonedaUSA getMoneda() {
 		return this.moneda;
 	}
 	
-	public Lista getLista() {
+	public ListaIngles getLista() {
 		return this.lista;
 	}
 
